8440cc0e3cded7155d5a2f16acbe961ae4fc46fe
Nick Hynes <nhynes@nhynes.com>
2022-03-28 15:42:13 +0800

    Add tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex justify-center">
+<h1 class="font-bold">Mint NFTs</h1>
+</main>

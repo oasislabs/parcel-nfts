debd060054eecde665cd7283329da3a89e862e77
Nick Hynes <nhynes@nhynes.com>
2022-04-01 00:38:09 +0800

    Add manifest format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,13 @@
   import { address as ethAddress, network as ethNetwork } from '../stores/eth';
 
   import { identity as parcelIdentity, connect as connectToParcel } from '../stores/parcel';
+
+  function processUploadBundle(e: Event) {
+    console.log(e);
+  }
 </script>
 
-<h1 class="font-bold text-center">Mint NFTs</h1>
+<h1 class="font-bold text-center text-xl">Mint NFTs</h1>
 {#if $error}
   <span class="text-red-500">{$error}</span>
 {/if}
@@ -34,13 +38,13 @@
       <hr />
       <div>
         <div>
-          <span>Please select the following files:</span>
+          <span>Please provide the following files:</span>
           <ul>
             <li>
               <span class="font-mono text-sm">manifest.json</span> - a JSON file describing the
               remaining inputs that has schema:
-              <pre class="my-0 border-2 border-black border-solid">
-type Manifest = NftDescriptor[]
+              <pre
+                class="my-2 border-0 border-l-4 border-gray-400 border-solid px-2 py-0">type Manifest = NftDescriptor[]
 
 type NftDescriptor {'{'}
   // The optional title of the NFT.
@@ -56,21 +60,17 @@ type NftDescriptor {'{'}
   // The name of the private (data) file.
   // Must be uploaded alongside the manifest.
   privateData: string
-{'}'}
-            </pre>
+{'}'}</pre>
             </li>
-            <li>
-              <pre class="inline">p</pre>
-              -
-            </li>
+            <li>all of the public images listed in the manifest</li>
+            <li>all of the private data listed in the manifest</li>
           </ul>
         </div>
-        <pre>
-&lt;nft_id&gt;_public.(png|jpg|...)
-&lt;nft_id&gt;_private.(txt|png|mov|cpp|...)
-        </pre>
       </div>
-      <form action="javascript:void(0)" />
+      <form on:submit|preventDefault={processUploadBundle}>
+        <input class="block my-2" type="file" multiple required />
+        <input class="block my-2" type="submit" value="Upload" />
+      </form>
     {/if}
   {:else}
     <p class="text-center">Please connect to the Emerald Testnet or Emerald Mainnet.</p>

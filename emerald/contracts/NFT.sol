234b7896c870fc7bdc86cc6a17db144a439971f2
Nick Hynes <nhynes@nhynes.com>
2022-04-01 17:07:18 +0800

    Add NFT contract

--- emerald/contracts/NFT.sol
+++ emerald/contracts/NFT.sol
@@ -1,29 +1,77 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721A, Ownable {
-    string public baseURI;
     uint256 public maxMintQty;
-    uint256 public premintPrice;
     uint256 public mintPrice;
+    uint256 public premintPrice;
+    mapping(address => bool) public premintList;
+    bool public hasBegunPublicSale;
+
+    string private baseURI;
+    bool private hasSetFinalBaseURI;
+
+    address payable private treasury;
 
     constructor(
-        string name,
-        string symbol,
-        uint256 maxMintQty,
-        uint256 premintPrice,
-        uint256 mintPrice
-    ) ERC721A(name, symbol) {}
-
-    function mint(uint256 quality) external payable {
-        // require(msg.value == )
-        _safeMint(msg.sender, quantity);
+        string memory _name,
+        string memory _symbol,
+        string memory _initialBaseURI,
+        uint256 _maxMintQty,
+        uint256 _premintPrice,
+        uint256 _mintPrice,
+        address payable _treasury
+    ) ERC721A(_name, _symbol) {
+        baseURI = _initialBaseURI;
+        require(_maxMintQty > 0, "maxMintQty == 0");
+        maxMintQty = _maxMintQty;
+        premintPrice = _premintPrice;
+        mintPrice = _mintPrice;
+        treasury = _treasury;
     }
 
-    function _baseURI() internal pure override returns (string memory) {
+    function mint(uint256 _quantity) external payable {
+        uint256 newNumMinted = _numberMinted(msg.sender) + _quantity;
+        require(newNumMinted <= maxMintQty, "num. minted exceeds max");
+
+        uint256 price = mintPrice;
+        if (premintList[msg.sender]) {
+            price = premintPrice;
+            if (newNumMinted == maxMintQty) {
+                delete premintList[msg.sender];
+            }
+        }
+        require(msg.value == price * _quantity, "incorrect payment amount");
+        treasury.transfer(msg.value);
+
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function addToPremintList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i; i < addresses.length; ++i) {
+            premintList[addresses[i]] = true;
+        }
+    }
+
+    function removeFromPremintList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i; i < addresses.length; ++i) {
+            delete premintList[addresses[i]];
+        }
+    }
+
+    function beginPublicSale() external onlyOwner {
+        hasBegunPublicSale = true;
+    }
+
+    function setFinalBaseURI(string calldata _finalBaseURI) external onlyOwner {
+        require(!hasSetFinalBaseURI, "final baseURI already set");
+        baseURI = _finalBaseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 }
---
+++ emerald/src/index.ts
@@ -0,0 +1,2 @@
+export { NFT } from '../typechain-types/NFT';
+export { NFT__factory as NFTFactory } from '../typechain-types/factories/NFT__factory';

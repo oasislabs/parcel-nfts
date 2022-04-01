b2407a42853dc42960ba9a2c790c8e5ccdfa0580
Nick Hynes <nhynes@nhynes.com>
2022-04-01 17:07:18 +0800

    Add NFT contract

--- emerald/lib/hardhat.config.d.ts
+++
@@ -1,7 +0,0 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import 'hardhat-watcher';
-import 'solidity-coverage';
-declare const config: HardhatUserConfig;
-export default config;

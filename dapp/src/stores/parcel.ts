af2f873cb4fa4aabb334af72c7ec478fbbab10e3
Nick Hynes <nhynes@nhynes.com>
2022-04-07 13:28:19 +0800

    Fix dapp lints and use new NFT contract

--- dapp/src/stores/parcel.ts
+++ dapp/src/stores/parcel.ts
@@ -27,37 +27,40 @@ function makeParcel(ethAddr: string): Parcel {
   });
 }
 
-const identityStore: Writable<Identity | undefined> = writable(undefined, function start(set) {
-  return parcelStore.subscribe(async (parcel) => {
-    if (!parcel) {
-      set(undefined);
-      return;
-    }
-    try {
-      set(await parcel.getCurrentIdentity());
-      return;
-    } catch (e: any) {
-      if (e?.response?.status !== 404) {
-        error.set('Failed to fetch Parcel identity. Please try again later.');
-        console.error('failed to fetch Parcel identity', e);
+const identityStore: Writable<Identity | undefined> = writable(
+  undefined,
+  function start(set: (identity?: Identity) => void) {
+    return parcelStore.subscribe(async (parcel) => {
+      if (!parcel) {
+        set(undefined);
         return;
       }
-    }
-    // The identity does not exist, so create one.
-    try {
-      const signer = get(ethProvider).getSigner();
-      set(
-        await makeParcel().createIdentity({
-          ethAddress: get(ethAddress),
-          proof: await signer.signMessage('parcel.createIdentity'),
-        }),
-      );
-    } catch (e) {
-      error.set('Failed to create Parcel identity. Please try again later.');
-      console.error('failed to create Parcel identity', e);
-    }
-  });
-});
+      try {
+        set(await parcel.getCurrentIdentity());
+        return;
+      } catch (e: any) {
+        if (e?.response?.status !== 404) {
+          error.set('Failed to fetch Parcel identity. Please try again later.');
+          console.error('failed to fetch Parcel identity', e);
+          return;
+        }
+      }
+      // The identity does not exist, so create one.
+      try {
+        const signer = get(ethProvider)!.getSigner();
+        set(
+          await parcel.createIdentity({
+            ethAddress: get(ethAddress)!,
+            proof: await signer.signMessage('parcel.createIdentity'),
+          }),
+        );
+      } catch (e) {
+        error.set('Failed to create Parcel identity. Please try again later.');
+        console.error('failed to create Parcel identity', e);
+      }
+    });
+  },
+);
 export const identity = unwritable(identityStore);
 
 export async function connect() {

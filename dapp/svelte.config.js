f20bc69640b30db34d293e5cabee527222085a8d
Nick Hynes <nhynes@nhynes.com>
2022-04-08 18:19:58 +0800

    Prepare for pre-release

--- dapp/svelte.config.js
+++ dapp/svelte.config.js
@@ -8,6 +8,9 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
+    prerender: {
+      default: true,
+    },
     adapter: adapter(),
     vite: {
       optimizeDeps: {
@@ -16,6 +19,11 @@ const config = {
       server: {
         fs: {
           allow: ['./emerald/lib'],
+        },
+      },
+      build: {
+        rollupOptions: {
+          external: ['node-fetch'],
         },
       },
     },

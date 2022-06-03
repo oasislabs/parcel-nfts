94a11c5afbcefa5afdf2351a96e754f056623c28
Nick Hynes <nhynes@nhynes.com>
2022-06-03 14:27:32 +0800

    Update deps

--- emerald/src/index.ts
+++ emerald/src/index.ts
@@ -1,5 +1,5 @@
-export { NFT } from '../typechain-types/NFT';
-export { NFT__factory as NFTFactory } from '../typechain-types/factories/NFT__factory';
+export { NFT } from '../typechain-types/contracts/NFT';
+export { NFT__factory as NFTFactory } from '../typechain-types/factories/contracts/NFT__factory';
 
-export { RevenueShare } from '../typechain-types/RevenueShare';
-export { RevenueShare__factory as RevenueShareFactory } from '../typechain-types/factories/RevenueShare__factory';
+export { RevenueShare } from '../typechain-types/contracts/RevenueShare';
+export { RevenueShare__factory as RevenueShareFactory } from '../typechain-types/factories/contracts/RevenueShare__factory';

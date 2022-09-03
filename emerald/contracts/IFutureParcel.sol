16f303900d8e6c3c01c65145638ecf42d1267e55
Nick Hynes <nhynes@nhynes.com>
2022-09-03 20:25:08 +0800

    emerald: add IFutureParcel.sol

---
+++ emerald/contracts/IFutureParcel.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+interface IFutureParcelNFT is IERC165 {
+    /// @dev This function should have semantics like `parcelTokenIds[i] = tokenIds[i]`.
+    /// @dev This function should also generally be access controlled (e.g., `onlyOwner`).
+    /// @param tokenIds A monotinically increasing list of NFT token IDs
+    /// @param parcelTokenIds A Parcel token ID.
+    function setParcelTokens(
+        uint256[] calldata tokenIds,
+        uint256[] calldata parcelTokenIds
+    ) external;
+
+    /// @notice Returns the Parcel token ID associated with the NFT with token ID `tokenId`.
+    function getParcelToken(uint256 tokenId) external view;
+}
